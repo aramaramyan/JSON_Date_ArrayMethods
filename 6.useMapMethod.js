43d6f441174a6980a116ba8efbb48f9c40934605
aramaramyan <aramaramyan1997@mail.ru>
2021-12-09 21:56:45 +0400

    fix(6.useMapMethod.js): fix bugs

--- 6.useMapMethod.js
+++ 6.useMapMethod.js
@@ -2,89 +2,97 @@
 
 // Your functions must use map method.
 
-{
-	// 1. Write a function which returns array of usernames.
+// 1. Write a function which returns array of usernames.
 
-	const users = [
-		{
-			username: "Yuri Gagarin",
-			lang: "ru",
-		},
-		{
-			username: "Nil Armstrong",
-			lang: "ENG",
-		},
-	];
+const users1 = [
+	{
+		username: "Yuri Gagarin",
+		lang: "ru",
+	},
+	{
+		username: "Nil Armstrong",
+		lang: "ENG",
+	},
+];
 
-	function getUserNames(obj) {
-		if (typeof (obj) !== "object" || obj === null || !Array.isArray(obj)) {
-			return "The argument of function mut be an object!";
-		}
+function getUserNames(arr) {
+	if (arguments.length !== 1 || !Array.isArray(arr)) {
+		return "The argument of function mut be an arry with objects in it!";
+	}
 
-		const result = obj.map(person => {
-			if (typeof (person.username) === "string") {
-				return person.username;
-			} else {
-				return null;
-			}
-		});
-
-		if (result.includes(undefined)) {
-			return `User at index:${result.indexOf(null)} has no username!`;
-		} else if (result.includes(null)) {
-			return `Username type on index: ${result.indexOf(null)} is not a string!`;
-		} else {
-			return result;
+	for (let i = 0; i < arr.length; i++) {
+		if (typeof (arr[i]) !== "object" || arr[i] === null || Array.isArray(arr[i])) {
+			return `Array item on index: ${i} is not an object!`
 		}
 	}
 
-	console.log("Task 1: ", getUserNames(users)); // ['Yuri Gagarin', 'Nil Armstrong']
+	const result = arr.map(person => {
+		if (person.username) {
+			return (typeof (person.username) !== "string") ? null : person.username;
+		} else {
+			return undefined;
+		}
+	});
+
+
+	if (result.includes(undefined)) {
+		return `User at index:${result.indexOf(undefined)} has no username!`;
+	} else if (result.includes(null)) {
+		return `Username type on index: ${result.indexOf(null)} is not a string!`;
+	} else {
+		return result;
+	}
 }
 
+console.log("Task 1: ", getUserNames(users1)); // ['Yuri Gagarin', 'Nil Armstrong']
 
-{
-	// 2. Write a function which returns array of lengths of user names.
+// 2. Write a function which returns array of lengths of user names.
 
-	const users = [
-		{
-			username: "Yuri Gagarin",
-			lang: "ru",
-		},
-		{
-			username: "Nil Armstrong",
-			lang: "ENG",
-		},
-	];
+const users2 = [
+	{
+		username: "Yuri Gagarin",
+		lang: "ru",
+	},
+	{
+		username: "Nil Armstrong",
+		lang: "ENG",
+	},
+];
 
-	function getUsernameLengths(obj) {
-		if (typeof (obj) !== "object" || obj === null || !Array.isArray(obj)) {
-			return "The argument of function mut be an object!";
-		}
+function getUsernameLengths(arr) {
+	if (arguments.length !== 1 || !Array.isArray(arr)) {
+		return "The argument of function mut be an arry with objects in it!";
+	}
 
-		const result = obj.map(person => {
-			if (typeof (person.username) === "string") {
-				return person.username.length;
-			} else {
-				return null;
-			}
-		});
-
-		if (result.includes(undefined)) {
-			return `User at index:${result.indexOf(null)} has no username!`;
-		} else if (result.includes(null)) {
-			return `Username type on index: ${result.indexOf(null)} is not a string!`;
-		} else {
-			return result;
+	for (let i = 0; i < arr.length; i++) {
+		if (typeof (arr[i]) !== "object" || arr[i] === null || Array.isArray(arr[i])) {
+			return `Array item on index: ${i} is not an object!`
 		}
 	}
 
-	console.log("Task 2: ", getUsernameLengths(users)); // [12, 13]
+	const result = arr.map(person => {
+		if (person.username) {
+			return (typeof (person.username) !== "string") ? null : person.username.length;
+		} else {
+			return undefined;
+		}
+	});
+
+	if (result.includes(undefined)) {
+		return `User at index:${result.indexOf(undefined)} has no username!`;
+	} else if (result.includes(null)) {
+		return `Username type on index: ${result.indexOf(null)} is not a string!`;
+	} else {
+		return result;
+	}
 }
+
+console.log("Task 2: ", getUsernameLengths(users2)); // [12, 13]
 
 // 3. Write a function which parses string integers. If it's not possible to parse, then add null.
 
 function parseInteger(arr) {
-	if (!Array.isArray(arr)) {
+	if (arguments.length !== 1 || !Array.isArray(arr)) {
 		return "The argumnet of function mut be an array!"
 	}
 
