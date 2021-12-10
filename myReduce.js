2ac1389e34748dea9804cdd65e5809d09c2abe7c
aramaramyan <aramaramyan1997@mail.ru>
2021-12-10 21:57:23 +0400

    feat: create myReduce.js file and resolve task

---
+++ myReduce.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// Implement array Reduce method.
+
+function myReduce(arr, func, val) {
+	let aggr = val;
+	let index = 0;
+
+	if (val === undefined) {
+		aggr = arr[0];
+		index = 1;
+	}
+
+	for (let i = index; i < arr.length; i++) {
+		aggr = func(aggr, arr[i], i, arr);
+	}
+
+	return aggr;
+}
+
+console.log(myReduce([1, 2, 3], (aggr, val) => aggr + val, 0)); // 6
