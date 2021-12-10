94f22c79e81a0801238de7ac8048c1a49514a8d1
aramaramyan <aramaramyan1997@mail.ru>
2021-12-10 20:59:50 +0400

    fix(myForEach.js): fix some details

--- myForEach.js
+++ myForEach.js
@@ -7,13 +7,9 @@ function myForEach(arr, func) {
 		return "The first argument of function mut be an arry and the second - call back function!";
 	}
 
-	const newArr = [];
-
 	for (let i = 0; i < arr.length; i++) {
-		newArr[i] = func(arr[i]);
+		func(arr[i]);
 	}
-
-	return newArr;
 }
 
-console.log(myForEach([1, 2, 3], item => item += 5)); // [6, 7, 8]
+myForEach([1, 2, 3], item => console.log(item += 5)); // 6 7 8
